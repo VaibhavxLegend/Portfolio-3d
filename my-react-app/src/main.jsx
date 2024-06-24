3379375e48389fcf7b2911ff729a2188d9345dd9
Vaibhav Jain <88039285+VaibhavxLegend@users.noreply.github.com>
2024-06-25 00:15:30 +0530

    test

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Nav } from './Nav_bar.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Nav />
   </React.StrictMode>,
 )
