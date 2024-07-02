0a7ca08a32742b649952c8f083ef42cdb9530601
Vaibhav Jain <88039285+VaibhavxLegend@users.noreply.github.com>
2024-07-02 21:48:12 +0530

    new updates

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react'
+import { Link } from 'react-router-dom'
+import {styles} from '../styles'
+import { NavLink } from '../constants'
+import { logo,menu,close } from '../assets'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full items-center py-5 fixed top-0 z-20`}>
+      <div className="flex items-center justify-between w-full max-w-7x1 mx-auto">
+        <Link to="/" className='flex items-center gap-2' onClick={()=> {setActive("");window.scrollTo(0,0); }}>
+        <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
